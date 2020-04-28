b5fda7132dd607d0b2f654410a4f17e6b85349bd
Bobby Laudeman <bobbylaudeman@gmail.com>
2020-04-28 15:02:23 -0700

    Initial Commit

---
+++ Swift Tutorial/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Swift Tutorial
+//
+//  Created by Bobby Laudeman on 4/28/20.
+//  Copyright © 2020 Bobby Laudeman. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
